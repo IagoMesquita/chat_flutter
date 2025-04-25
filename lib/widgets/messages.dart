1fbf092c038de497a0cf589831df1061335534c7
Iago Mesquita <iagomesquitaalves@gmail.com>
2025-04-25 08:39:00 -0300

    feat: return messages in widget messages

--- lib/widgets/messages.dart
+++ lib/widgets/messages.dart
@@ -1,3 +1,7 @@
+import 'dart:math';
+
+import 'package:chat_2025/core/model/chat_message.dart';
+import 'package:chat_2025/core/service/chat/chat_service.dart';
 import 'package:flutter/material.dart';
 
 class Messages extends StatelessWidget {
@@ -5,8 +9,27 @@ class Messages extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Nova Messagem'),
+    return StreamBuilder<List<ChatMessage>>(
+      stream: ChatService().messagesStream(),
+      builder: (context, snapshot) {
+        if(snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator(),);
+        } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+          return const Center(child: Text('Sem menssagens! Vamos conversar?'),);
+        } else {
+          final messages = snapshot.data!;
+
+          return ListView.builder(
+            reverse: true,
+            itemCount: messages.length,
+            itemBuilder: (context, index) {
+              return Text(messages[index].text);
+            },
+            // itemBuilder: messages,
+          );
+        }
+      },
     );
+    
   }
 }
