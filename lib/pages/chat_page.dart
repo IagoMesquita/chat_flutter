b2d9a848912a7b6a3ee33a7f94a4c3253a608f25
Iago Mesquita <iagomesquitaalves@gmail.com>
2025-04-25 07:23:01 -0300

    feat: add inital chat page

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -1,4 +1,6 @@
 import 'package:chat_2025/core/service/auth/auth_service.dart';
+import 'package:chat_2025/widgets/messages.dart';
+import 'package:chat_2025/widgets/new_message.dart';
 import 'package:flutter/material.dart';
 
 class ChatPage extends StatelessWidget {
@@ -7,20 +9,53 @@ class ChatPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Center(
+      appBar: AppBar(
+        title: const Center(
             child: Text(
-              'Chat Page',
-              style: TextStyle(fontSize: 20),
+          'Cod3r Chat',
+        )),
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        titleTextStyle: const TextStyle(
+          color: Colors.white,
+          fontSize: 25,
+          fontWeight: FontWeight.bold,
+        ),
+        actions: [
+          DropdownButton(
+            icon: const Icon(
+              Icons.more_vert,
+              color: Colors.white,
             ),
-          ),
-          TextButton(
-            onPressed: AuthService().logout,
-            child: const Text('Deslogar'),
+            items: const [
+              DropdownMenuItem(
+                  value: 'logout',
+                  child: Row(
+                    children: [
+                      Icon(
+                        Icons.exit_to_app,
+                        color: Colors.black87,
+                      ),
+                      SizedBox(width: 10,),
+                      Text('Sair')
+                    ],
+                  ))
+            ],
+            onChanged: (value) {
+              if(value == 'logout') {
+                AuthService().logout();
+              }
+            },
           )
         ],
+      ),
+      body: const SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Expanded(child: Messages()),
+            NewMessage(),
+          ],
+        ),
       ),
     );
   }
