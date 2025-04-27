b50a1d682da797e945ddfcfcbe1e8bd91967b6c5
Iago Mesquita <iagomesquitaalves@gmail.com>
2025-04-26 21:02:20 -0300

    featL add message Bubbles in messages

--- lib/widgets/messages.dart
+++ lib/widgets/messages.dart
@@ -1,7 +1,9 @@
 import 'dart:math';
 
 import 'package:chat_2025/core/model/chat_message.dart';
+import 'package:chat_2025/core/service/auth/auth_service.dart';
 import 'package:chat_2025/core/service/chat/chat_service.dart';
+import 'package:chat_2025/widgets/message_bubble.dart';
 import 'package:flutter/material.dart';
 
 class Messages extends StatelessWidget {
@@ -9,27 +11,36 @@ class Messages extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final currentUser = AuthService().currentUser;
+
     return StreamBuilder<List<ChatMessage>>(
       stream: ChatService().messagesStream(),
       builder: (context, snapshot) {
-        if(snapshot.connectionState == ConnectionState.waiting) {
-          return const Center(child: CircularProgressIndicator(),);
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
         } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-          return const Center(child: Text('Sem menssagens! Vamos conversar?'),);
+          return const Center(
+            child: Text('Sem menssagens! Vamos conversar?'),
+          );
         } else {
           final messages = snapshot.data!;
 
           return ListView.builder(
             reverse: true,
             itemCount: messages.length,
-            itemBuilder: (context, index) {
-              return Text(messages[index].text);
+            itemBuilder: (context, i) {
+              return MessageBubble(
+                key: ValueKey(messages[i].id),
+                message: messages[i],
+                belongsToCurrentUser: currentUser?.id == messages[i].userId,
+              );
             },
             // itemBuilder: messages,
           );
         }
       },
     );
-    
   }
 }
