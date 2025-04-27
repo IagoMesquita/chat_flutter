61cd33e984691e575cb58e384398dfafc147d108
Iago Mesquita <iagomesquitaalves@gmail.com>
2025-04-26 22:17:17 -0300

    feat: add text message and send message

--- lib/widgets/new_message.dart
+++ lib/widgets/new_message.dart
@@ -1,12 +1,48 @@
+import 'package:chat_2025/core/service/auth/auth_service.dart';
+import 'package:chat_2025/core/service/chat/chat_service.dart';
 import 'package:flutter/material.dart';
 
-class NewMessage extends StatelessWidget {
+class NewMessage extends StatefulWidget {
   const NewMessage({super.key});
 
   @override
+  State<NewMessage> createState() => _NewMessageState();
+}
+
+class _NewMessageState extends State<NewMessage> {
+  String _message = '';
+  final _messageController = TextEditingController();
+
+  Future<void> _sendMessage() async {
+    final user = AuthService().currentUser;
+
+    if (user != null) {
+      await ChatService().save(_message, user);
+      _messageController.clear();
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Messagens'),
+    return Row(
+      children: [
+        Expanded(
+          child: TextField(
+            controller: _messageController,
+            onChanged: (msg) => setState(() => _message = msg),
+            decoration: const InputDecoration(labelText: 'Enviar menssagem...'),
+            onSubmitted: (_) {
+              if(_message.trim().isEmpty) {
+                _sendMessage();
+              }
+            },
+          ),
+        ),
+        IconButton(
+          icon: const Icon(Icons.send),
+          onPressed: _message.trim().isEmpty ? null : _sendMessage,
+        )
+      ],
     );
   }
 }
