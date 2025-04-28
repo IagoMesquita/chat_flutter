2e2c58a3980e419cd361f06c5035a64e7ed21062
Iago Mesquita <iagomesquitaalves@gmail.com>
2025-04-28 07:48:21 -0300

    feat: add service push notification with ChangeNotifier

---
+++ lib/core/service/notification/push_notification_service.dart
@@ -0,0 +1,19 @@
+import 'package:chat_2025/core/model/chat_notification.dart';
+import 'package:flutter/material.dart';
+
+class PushNotificationService with ChangeNotifier {
+  final List<ChatNotification> _items = [];
+
+  List<ChatNotification> get items {
+    return [..._items];
+  }
+
+  void add(ChatNotification notification) {
+    _items.add(notification);
+    notifyListeners();
+  }
+
+  void remove(int i) {
+    _items.removeAt(i);
+  }
+}
