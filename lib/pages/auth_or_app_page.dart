1b1bc2cc1d70d81341432f559f51c7089893127d
Iago Mesquita <iagomesquitaalves@gmail.com>
2025-05-01 11:10:02 -0300

    feat: initialize firebase in author or app page

--- lib/pages/auth_or_app_page.dart
+++ lib/pages/auth_or_app_page.dart
@@ -4,23 +4,36 @@ import 'package:chat_2025/pages/auth_page.dart';
 import 'package:chat_2025/pages/chat_page.dart';
 import 'package:chat_2025/pages/loading_page.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 class AuthOrAppPage extends StatelessWidget {
   const AuthOrAppPage({super.key});
 
+  Future<void> init(BuildContext context) async {
+    await Firebase.initializeApp();
+    print('Fire base inicializado');
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: StreamBuilder<ChatUser?>(
-        stream: AuthService().userChanges,
+    return FutureBuilder(
+        future: init(context),
         builder: (context, snapshot) {
-          if(snapshot.connectionState == ConnectionState.waiting) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
             return const LoadingPage();
-          }else {
-            return snapshot.hasData ? const ChatPage() : const AuthPage();
+          } else {
+            return StreamBuilder<ChatUser?>(
+            stream: AuthService().userChanges,
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const LoadingPage();
+              } else {
+                return snapshot.hasData ? const ChatPage() : const AuthPage();
+              }
+            },
+          );
           }
-        } ,
-      ),
-    );
+          
+        });
   }
 }
